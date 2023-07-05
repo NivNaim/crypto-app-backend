7dbb2053ccc22d92e62a2a3303df945c1f52b14c
NivNaim <nivnaimwork@gmail.com>
2023-07-06 01:28:03 +0300

    Delete users-service directory

--- users-service/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import config from "config";
-
-const app = express();
-const port = config.get("app.port");
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
