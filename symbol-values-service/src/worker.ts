d98a9135d6ba3bab8b14c3dc3f88ae498c52874c
Niv Naim <nivnaimwork@gmail.com>
2023-07-13 01:57:42 +0300

    feat: create socket server in user-symbols service

--- symbol-values-service/src/worker.ts
+++ symbol-values-service/src/worker.ts
@@ -2,17 +2,32 @@ import config from "config";
 import axios from "axios";
 import io, { Socket } from "socket.io-client";
 
-import SymbolValue from "./models/symbol-value";
 import mongoConnection from "./middlewares/mongo";
+import UserSymbol from "./models/user-symbol";
+import SymbolValue from "./models/symbol-value";
 
-const fetchExchangeRates = async (baseCurrency: string): Promise<any> => {
+const fetchCryproRates = async (
+  symbol: string,
+  socket: Socket
+): Promise<any> => {
   try {
     const response = await axios.get(
-      `https://v6.exchangerate-api.com/v6/${config.get(
-        "exchange-rates.api-key"
-      )}/latest/${baseCurrency}`
+      `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=${config.get(
+        "cryptocompare.base-currency"
+      )}&api_key=${config.get("cryptocompare.api-key")}`
     );
-    return response.data;
+
+    const symbolValue = new SymbolValue({
+      symbol: symbol,
+      value: response.data.USD,
+    });
+    await symbolValue.save();
+
+    socket.emit("message from worker", {
+      symbol: symbolValue.symbol,
+      value: symbolValue.value,
+    });
+    console.log(`saved ${symbolValue.value} for ${symbolValue.symbol}`);
   } catch (error) {
     console.log("Error fetching exchange rates:", error);
     throw error;
@@ -20,32 +35,13 @@ const fetchExchangeRates = async (baseCurrency: string): Promise<any> => {
 };
 
 const loop = async (socket: Socket): Promise<void> => {
-  const symbols = ["ILS", "EUR", "GBP"];
+  const symbols = await UserSymbol.distinct("symbol").exec();
   console.log(`loop: found this symbol array: ${symbols}`);
 
-  try {
-    const baseCurrency: string = config.get("exchange-rates.base-currency");
-    const exchangeRates = await fetchExchangeRates(baseCurrency);
-
-    symbols.forEach((symbol) => {
-      if (
-        exchangeRates &&
-        exchangeRates.conversion_rates &&
-        exchangeRates.conversion_rates[symbol]
-      ) {
-        const value = exchangeRates.conversion_rates[symbol];
-        socket.emit("message from worker", {
-          symbol: symbol,
-          value: value,
-        });
-        console.log(`Emitted ${value} for ${symbol}`);
-      } else {
-        console.log(`No exchange rate found for ${symbol}`);
-      }
-    });
-  } catch (error) {
-    console.error("Error fetching exchange rates:", error);
-  }
+  const promises = symbols.map((symbol: string) =>
+    fetchCryproRates(symbol, socket)
+  );
+  await Promise.allSettled(promises);
 
   setTimeout(() => loop(socket), config.get("worker.interval"));
 };
