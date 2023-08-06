18d9b5db757f349d83e09c7b18614605ba4d77b2
NivNaim <nivnaimwork@gmail.com>
2023-08-06 03:07:40 +0300

    refactor: change in default.json

--- authentication-service/src/types/symbol-value.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-import { ISymbol } from "./symbol";
-
-export interface ISymbolValue extends Document {
-  symbol: ISymbol["_id"];
-  value: number;
-  createdAt: Date;
-}
--- authentication-service/src/types/symbol.d.ts
+++
@@ -1,5 +0,0 @@
-import { Document, Schema } from "mongoose";
-
-export interface ISymbol extends Document {
-  symbol: string;
-}
