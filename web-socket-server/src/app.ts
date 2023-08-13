131739c28a2bb502bed9157fc050ef1c749d9ef9
NivNaim <nivnaimwork@gmail.com>
2023-08-13 19:39:53 +0300

    refactor: refactor to socket server

--- web-socket-server/src/app.ts
+++ web-socket-server/src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import axios from "axios";
-import mongoose from "mongoose";
 import config from "config";
 import Symbol from "./models/symbol";
 import { get400, get404, get500 } from "./middlewares/error";
 import { ISymbol } from "./types/symbol";
 import SymbolValue from "./models/symbol-value";
 import dbConnection from "./db/connection";
+import { ISymbolValue } from "./types/symbol-value";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
 const app = express();
 const port = config.get("socket-server.port");
@@ -30,68 +31,93 @@ app.use(get500);
 const server = http.createServer(app);
 const io = new Server(server, { cors: corsOptions });
 
-const fetchAndSaveCryptoRates = async (symbol: string): Promise<any> => {
+const fetchRealTimeValue = async (symbol) => {
   try {
     const response = await axios.get(
       `https://min-api.cryptocompare.com/data/price`,
       {
         params: {
           fsym: symbol,
-          tsyms: "USD",
+          tsyms: `${config.get("cryptocompare.base-currency")}`,
           api_key: config.get("cryptocompare.api-key"),
         },
       }
     );
-
-    const symbolDocument = await Symbol.findOne({ symbol });
-    if (!symbolDocument) {
-      throw new Error(`Symbol not found for ${symbol}`);
-    }
-
-    const currentDateTime = new Date()
-      .toISOString()
-      .replace("T", " ")
-      .slice(0, 19);
-
-    const symbolValue = {
-      symbol: symbolDocument._id,
+    const currentDateTime = new Date().toISOString();
+    return {
+      symbol: symbol,
       value: response.data.USD,
       dateTime: currentDateTime,
     };
-
-    await SymbolValue.create(symbolValue);
-
-    io.emit("data", symbolValue);
-
-    return symbolValue;
   } catch (error) {
-    console.log(`Error fetching exchange rates for ${symbol}:`, error);
+    console.log(`Error fetching real-time value for ${symbol}:`, error);
     throw error;
   }
 };
 
-const loop = async (): Promise<void> => {
+const fetchHistoricalValues = async (symbol) => {
   try {
-    const symbolsDocuments: ISymbol[] = await Symbol.find();
-    const symbols: string[] = symbolsDocuments.map(
-      (symbolDocument) => symbolDocument.symbol
+    const response = await axios.get(
+      `https://min-api.cryptocompare.com/data/v2/histoday`,
+      {
+        params: {
+          fsym: symbol,
+          tsym: `${config.get("cryptocompare.base-currency")}`,
+          limit: 7,
+          api_key: config.get("cryptocompare.api-key"),
+        },
+      }
     );
-    console.log(`loop: found this symbol array: ${symbols}`);
-
-    const promises = symbols.map((symbol: string) =>
-      fetchAndSaveCryptoRates(symbol)
-    );
-    await Promise.allSettled(promises);
-
-    setTimeout(loop, loopTimeout);
-  } catch (err) {
-    console.log("Error in loop:", err);
+    const historicalData = response.data.Data.Data;
+    return historicalData.map((entry: { time: number; close: any }) => ({
+      date: new Date(entry.time * 1000).toISOString().split("T")[0],
+      value: entry.close,
+    }));
+  } catch (error) {
+    console.log(`Error fetching historical values for ${symbol}:`, error);
+    throw error;
   }
 };
+
+const emitRealTimeValues = async (socket: Socket) => {
+  const symbolsDocuments = await Symbol.find();
+  const realTimeValues = await Promise.all(
+    symbolsDocuments.map((symbolDocument) =>
+      fetchRealTimeValue(symbolDocument.symbol)
+    )
+  );
+  socket.emit("realTimeValues", realTimeValues);
+};
+
+const emitLastWeekValues = async (socket) => {
+  const symbolsDocuments = await Symbol.find();
+  const lastWeekValues = await Promise.all(
+    symbolsDocuments.map((symbolDocument) =>
+      fetchHistoricalValues(symbolDocument.symbol)
+    )
+  );
+  const symbolData = {};
+  symbolsDocuments.forEach((symbolDocument, index) => {
+    const symbol = symbolDocument.symbol;
+    symbolData[symbol] = lastWeekValues[index];
+  });
+  socket.emit("lastWeekValues", symbolData);
+};
+
+io.on("connection", async (socket) => {
+  console.log("Socket client connected:", socket.id);
+
+  await emitRealTimeValues(socket);
+
+  await emitLastWeekValues(socket);
+
+  setInterval(async () => {
+    await emitLastWeekValues(socket);
+  }, 24 * 60 * 60 * 1000);
+});
 
 dbConnection().then(() => {
   server.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    loop();
   });
 });
